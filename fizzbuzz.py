dcbaa3dc936deee7171922fb7aa157ccc15fa2d5
Jim Zhu <jyz6@case.edu>
2018-01-21 14:52:37 -0500

    prints bizz when blah

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 def fizzbuzz(n):
-    for i in range(n):
+    for i in range(1, n + 1):
         if i % 15 == 0:
             print "fizzbuzz"
         elif i % 3 == 0:
             print "fizz"
         elif i % 5 == 0:
             print "buzz"
+	elif i % 7 == 0:
+            print "bizz"
         else:
             print i
